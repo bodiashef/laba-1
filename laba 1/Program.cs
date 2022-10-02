05f490a36ef8bd2e0ff00066162a16275b8821f3
bodja <bodja@DESKTOP-RP2SRO4>
2022-10-02 13:47:42 +0300

    Add project files.

---
+++ laba 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    
+    
+        static void Main(string[] args)
+        {
+        // task //1.1//
+            int day, month;
+            day = Convert.ToInt32(Console.ReadLine());
+            month = Convert.ToInt32(Console.ReadLine());
+            string result = (day > 0) && (day < 31) && (month > 0) && (month < 12) ?
+            "Значення можуть представляти день і місяць" : "Значення не можуть представляти день і місяць";
+            Console.WriteLine(result);
+
+        
+    
+             // task //1.2//
+
+        }
+
+
+
+}
